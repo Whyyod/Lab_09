8c7156c22fb569dfb2dfa926b9eefbd8c6b4e07a
Whyyod <Yodsanon_sri@cmu.ac.th>
2023-12-15 16:21:59 +0700

    Complete

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,14 +2,19 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double 	PrevBalance , interest , Payment , Total , NewBalance ;
 	cout << "Enter initial loan: ";
+	cin >> PrevBalance ;
 	cout << "Enter interest rate per year (%): ";
+	cin  >> interest ;
 	cout << "Enter amount you can pay per year: ";
+	cin >> Payment ;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
+
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
